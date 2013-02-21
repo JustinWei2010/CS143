34441828cb0f0f7bf4e1cffe961ab9728bfd449e
HotFro <hottfro@gmail.com>
2013-02-20 16:49:33 -0800

    Combined work so far

--- lab2/BTreeNode.cc
+++ lab2/BTreeNode.cc
@@ -194,7 +194,8 @@ BTNonLeafNode::BTNonLeafNode()
  * @param pf[IN] PageFile to read from
  * @return 0 if successful. Return an error code if there is an error.
  */
-RC BTNonLeafNode::read(PageId pid, const PageFile& pf){
+RC BTNonLeafNode::read(PageId pid, const PageFile& pf)
+{
 	return pf.read(pid, buffer); 
 }
     
@@ -204,7 +205,8 @@ RC BTNonLeafNode::read(PageId pid, const PageFile& pf){
  * @param pf[IN] PageFile to write to
  * @return 0 if successful. Return an error code if there is an error.
  */
-RC BTNonLeafNode::write(PageId pid, PageFile& pf){ 
+RC BTNonLeafNode::write(PageId pid, PageFile& pf)
+{ 
 	return pf.write(pid, buffer);
 }
 
@@ -213,7 +215,9 @@ RC BTNonLeafNode::write(PageId pid, PageFile& pf){
  * @return the number of keys in the node
  */
 int BTNonLeafNode::getKeyCount()
-{ return tupleCount; }
+{
+	return tupleCount; 
+}
 
 
 /*
