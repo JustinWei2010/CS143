807e28eaaf686bc1c08870a5640c8f4b6c9296a9
Nathan Lai <nathan.lai@sbcglobal.net>
2013-03-03 13:53:30 -0800

    SqlEngine: Added tabs to select + Changed Implementation of Insert
    Still thinking about how to implement select, think I almost got it.

--- lab2/SqlEngine.cc
+++ lab2/SqlEngine.cc
@@ -47,98 +47,120 @@ RC SqlEngine::run(FILE* commandline)
 
 RC SqlEngine::select(int attr, const string& table, const vector<SelCond>& cond)
 {
-  RecordFile rf;   // RecordFile containing the table
-  RecordId   rid;  // record cursor for table scanning
+	RecordFile rf;   // RecordFile containing the table
+	RecordId   rid;  // record cursor for table scanning
 
-  RC     rc;
-  int    key;     
-  string value;
-  int    count;
-  int    diff;
+	RC     rc;
+	int    key;     
+	string value;
+	int    count;
+	int    diff;
+	
+	//variables for index
+	BTreeIndex treeIndex;
+	IndexCursor indxCursor;
 
-  // open the table file
-  if ((rc = rf.open(table + ".tbl", 'r')) < 0) {
-    fprintf(stderr, "Error: table %s does not exist\n", table.c_str());
-    return rc;
-  }
+	// open the table file
+	if ((rc = rf.open(table + ".tbl", 'r')) < 0) {
+		fprintf(stderr, "Error: table %s does not exist\n", table.c_str());
+		return rc;
+	}
+	
+	//check if there is an index
+	rc.read(rc.erid, key, value)
+	
+	// scan the table file from the beginning
+	rid.pid = rid.sid = 0;
+	count = 0;
+	
+	//if there is an index
+	if(key == 46339 && value.compare(0,5,"index") {
+		//define tree index's private functions
+			//incomplete
+		//read the cond to find out the key to search
+		treeIndex.locate(key, indxCursor);
+		//figure out how to get to the first rid
+		//use the Comparator to find out what to read out
+		//increment count
+		//print the tuple
+	}
+	
+	else {
+		// scan the table file from the beginning (w/o index)
+		while (rid < rf.endRid()) {
+			// read the tuple
+			if ((rc = rf.read(rid, key, value)) < 0) {
+				fprintf(stderr, "Error: while reading a tuple from table %s\n", table.c_str());
+				goto exit_select;
+			}
 
-  // scan the table file from the beginning
-  rid.pid = rid.sid = 0;
-  count = 0;
-  while (rid < rf.endRid()) {
-    // read the tuple
-    if ((rc = rf.read(rid, key, value)) < 0) {
-      fprintf(stderr, "Error: while reading a tuple from table %s\n", table.c_str());
-      goto exit_select;
-    }
+			// check the conditions on the tuple
+			for (unsigned i = 0; i < cond.size(); i++) {
+				// compute the difference between the tuple value and the condition value
+				switch (cond[i].attr) {
+					case 1:
+						diff = key - atoi(cond[i].value);
+						break;
+					case 2:
+						diff = strcmp(value.c_str(), cond[i].value);
+						break;
+				}
+				// skip the tuple if any condition is not met
+				switch (cond[i].comp) {
+					case SelCond::EQ:
+						if (diff != 0) goto next_tuple;
+						break;
+					case SelCond::NE:
+						if (diff == 0) goto next_tuple;
+						break;
+					case SelCond::GT:
+						if (diff <= 0) goto next_tuple;
+						break;
+					case SelCond::LT:
+						if (diff >= 0) goto next_tuple;
+						break;
+					case SelCond::GE:
+						if (diff < 0) goto next_tuple;
+						break;
+					case SelCond::LE:
+						if (diff > 0) goto next_tuple;
+						break;
+				}
+			}
 
-    // check the conditions on the tuple
-    for (unsigned i = 0; i < cond.size(); i++) {
-      // compute the difference between the tuple value and the condition value
-      switch (cond[i].attr) {
-      case 1:
-	diff = key - atoi(cond[i].value);
-	break;
-      case 2:
-	diff = strcmp(value.c_str(), cond[i].value);
-	break;
-      }
+			// the condition is met for the tuple. 
+			// increase matching tuple counter
+			count++;
 
-      // skip the tuple if any condition is not met
-      switch (cond[i].comp) {
-      case SelCond::EQ:
-	if (diff != 0) goto next_tuple;
-	break;
-      case SelCond::NE:
-	if (diff == 0) goto next_tuple;
-	break;
-      case SelCond::GT:
-	if (diff <= 0) goto next_tuple;
-	break;
-      case SelCond::LT:
-	if (diff >= 0) goto next_tuple;
-	break;
-      case SelCond::GE:
-	if (diff < 0) goto next_tuple;
-	break;
-      case SelCond::LE:
-	if (diff > 0) goto next_tuple;
-	break;
-      }
-    }
+			// print the tuple 
+			switch (attr) {
+				case 1:  // SELECT key
+					fprintf(stdout, "%d\n", key);
+					break;
+				case 2:  // SELECT value
+					fprintf(stdout, "%s\n", value.c_str());
+					break;
+				case 3:  // SELECT *
+					fprintf(stdout, "%d '%s'\n", key, value.c_str());
+					break;
+			}
 
-    // the condition is met for the tuple. 
-    // increase matching tuple counter
-    count++;
+			// move to the next tuple
+			next_tuple:
+			++rid;
+		}
+	}
+	
+	// print matching tuple count if "select count(*)"
+	if (attr == 4) {
+		fprintf(stdout, "%d\n", count);
+	}
+	rc = 0;
 
-    // print the tuple 
-    switch (attr) {
-    case 1:  // SELECT key
-      fprintf(stdout, "%d\n", key);
-      break;
-    case 2:  // SELECT value
-      fprintf(stdout, "%s\n", value.c_str());
-      break;
-    case 3:  // SELECT *
-      fprintf(stdout, "%d '%s'\n", key, value.c_str());
-      break;
-    }
-
-    // move to the next tuple
-    next_tuple:
-    ++rid;
-  }
-
-  // print matching tuple count if "select count(*)"
-  if (attr == 4) {
-    fprintf(stdout, "%d\n", count);
-  }
-  rc = 0;
-
-  // close the table file and return
-  exit_select:
-  rf.close();
-  return rc;
+	// close the table file and return
+	exit_select:
+	rf.close();
+	return rc;
 }
 
 RC SqlEngine::load(const string& table, const string& loadfile, bool index)
@@ -167,9 +189,9 @@ RC SqlEngine::load(const string& table, const string& loadfile, bool index)
 	}
 	
 	//read in file
+	int key;
+	string line, value;
 	while(!file.eof()){
-		int key;
-		string line, value;
 		getline(file, line);
 		if((rc = parseLoadLine(line, key, value)) < 0){
 			rf.close();
@@ -186,10 +208,10 @@ RC SqlEngine::load(const string& table, const string& loadfile, bool index)
 			}
 		}
 	}
-	//save treeIndex file constants before closing
-	rid.pid = treeIndex.rootPid;
-	rid.sid = treeIndex.treeHeight;
-	rf.erid = rid;
+	//save treeIndex file constants before closing (Needs fixing)
+	key = 46339; //index in telophone keypad numbers
+	value << "index," << rid.pid << "," << rid.sid; //adding on the tree information into the value
+	rf.append(key, value.str(), rid);
 	//close all of the files
 	treeIndex.close();
 	rf.close();
